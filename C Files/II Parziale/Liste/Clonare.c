e5af4fb27ff2c55e70c498ae245749b5fec11e9b
niccopeps <niccozeri23@gmail.com>
2025-01-12 11:52:26 +0100

    aggiunto file con clonazioni (per come l'abbiamo visto in classe e per com'è scritta nel libro. nel C code del moodle il prof dichiara come boolean e crea una variabile is_correct per vedere se la sufinsert avviene con successo) Merge con il main se te gusta

---
+++ C Files/II Parziale/Liste/Clonare.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+typedef unsigned short int boolean;
+#define TRUE 1
+#define FALSE 0
+
+struct list
+{
+    int value;
+    struct list *next;
+};
+
+void init(struct list **ptrptr)
+{
+    ptrptr = NULL;
+}
+
+boolean preinsert(struct list **ptrptr, int value)
+{
+    struct list *tmp;
+    tmp = (struct list *)malloc(sizeof(struct list));
+    if(tmp != NULL)
+    {
+        tmp->value = value;
+        tmp->next = *ptrptr;
+        *ptrptr = tmp;
+        return TRUE;
+    }
+    return FALSE;
+}
+
+boolean sufinsert(struct list **ptrptr, int value)
+{
+    while(*ptrptr != NULL)
+        ptrptr = &(*ptrptr)->next;
+    if(preinsert(ptrptr, value))
+        return TRUE;
+    return FALSE;
+}
+
+void clone(struct list *src, struct list **dst)
+{
+    init(dst);
+    while(src != NULL)
+    {
+        sufinsert(dst, src->value);
+        src = src->next;
+    }
+}
+
+void clone_lowcost(struct list *src, struct list **dst)
+{
+    init(dst);
+    while(src != NULL)
+    {
+        sufinsert(dst, src->value);
+        dst = &((*dst)->next);    //costo lineare grazie a questa riga che avanza la cella dell'array destinazione per evitare di scorrere tutte le volte elementi inutili.
+        src = src->next;
+    }
+}
